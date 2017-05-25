df899334c7567022d295dccdd9c6a7bf372bd3ec
Piotr Grzegorzewski <pgrzegorzewski.wenow@gmail.com>
2017-05-25 21:44:36 +0200

    Constants and template literals

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world! (from webpack) with update");
+let a='hello';
+
+console.log(a);
+
+{
+	let a = 'goodbye';
+	console.log('a inside scope is ', a);
+}
+
+
+const constArray = [1, 2, 3];
+constArray.push(4);
+console.log("Constant array can be manippulated: ", constArray);
+
+let b=`${a} world`;
+console.log("Template literals: ", b);
